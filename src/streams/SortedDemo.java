392a09751776c56255175d2a1fe82e37746fb4e9
Rajkumar Katanor <katanor.raj@gmail.com>
2022-12-20 22:50:03 -0600

    sorting

--- src/streams/SortedDemo.java
+++ src/streams/SortedDemo.java
@@ -5,13 +5,23 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortedDemo {
-public static void main(String[] args) {
-	List<Integer> list =Arrays.asList(1,2,3,4,5);
-	
-	List<String> strList=Arrays.asList("b","ab","aa","z");
-	
-	list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);
-	
-	strList.stream().sorted().forEach(System.out::println);
-}
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+		List<String> strList = Arrays.asList("b", "ab", "aa", "z");
+
+		list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);
+
+		strList.stream().sorted().forEach(System.out::println);
+
+		List<Employee> emp = Arrays.asList(new Employee(1, "raj", 400), new Employee(2, "lilly", 200),
+				new Employee(3, "chris", 600), new Employee(1, "john", 100));
+		
+		
+		//emp.stream().sorted((e1,e2)-> (int) (e2.getSal()-e1.getSal())).forEach(System.out::println);
+		
+		emp.stream().sorted(Comparator.comparing(Employee::getId)).forEach(System.out::println);
+		
+		//System.out.println(emp);
+	}
 }
